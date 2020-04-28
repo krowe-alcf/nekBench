a2df489b00b6cbf4a7b0bc09a975e05a2731e8b2
krowe-alcf <krowe@anl.gov>
2020-04-28 01:26:29 +0000

    Fixed bugs in axhelm and nekBone. Finished initial CMake build.

--- axhelm/kernelHelper.cpp
+++ axhelm/kernelHelper.cpp
@@ -31,7 +31,6 @@ static occa::kernel loadAxKernel(occa::device device, const std::string threadMo
   props["defines/dfloat"] = dfloatString;
   props["defines/dlong"]  = dlongString;
 
-  props["okl"] = false;
 
   occa::kernel axKernel;
 
@@ -39,14 +38,16 @@ static occa::kernel loadAxKernel(occa::device device, const std::string threadMo
   for (int r=0;r<2;r++){
     if ((r==0 && rank==0) || (r==1 && rank>0)) {
       if(strstr(threadModel.c_str(), "NATIVE+CUDA")){
+        props["okl"] = false;
         axKernel = device.buildKernel(filename + ".cu", kernelName, props);
         axKernel.setRunDims(Nelements, Nq*Nq);
       } else if(strstr(threadModel.c_str(), "NATIVE+SERIAL") || 
                 strstr(threadModel.c_str(), "NATIVE+OPENMP")){
+        props["okl"] = false;
         props["defines/USE_OCCA_MEM_BYTE_ALIGN"] = USE_OCCA_MEM_BYTE_ALIGN;
         axKernel = device.buildKernel(filename + ".c", kernelName, props);
       } else { // fallback is okl
-        props["okl"] = true;
+        //props["okl/enabled"] = true;
         axKernel = device.buildKernel(filename + ".okl", kernelName, props);
       }
     }
--- build-nekBench.sh
+++ build-nekBench.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-CC=mpicc
+set -x
+#-----
+CC=gcc
 CFLAGS=
 
-CXX=mpicxx
+CXX=g++
 CXXFLAGS=
 
-FC=mpifort
+FC=gfortran
 FFLAGS=
 
 MPICC=mpicc
 MPICXX=mpicxx
 MPIFC=mpifort
 
-#OpenCL_LIBRARY=/soft/libraries/public_opencl/opencl_19.47.14903/usr/lib64
-#OpenCL_LIBRARY=/soft/libraries/public_opencl/opencl_loader/ocl-icd/libOpenCL.so
-#OpenCL_LIBRARY=/soft/libraries/khronos/loader/master-26Mar20/lib64/libOpenCL.so.1
-#OpenCL_INCLUDE_DIRS=/soft/libraries/OpenCL-Headers
+ALLOW_OPENCL=ON
+OpenCL_LIBRARY=/soft/libraries/khronos/loader/master-26Mar20/lib64/libOpenCL.so.1
+OpenCL_INCLUDE_DIR=/soft/libraries/khronos/headers/master-26Mar20/include
 
+BUILD_DIR=build
 INSTALL_DIR=`pwd`/install
+#-----
+mkdir -p $BUILD_DIR
+mkdir -p $INSTALL_DIR
 
-mkdir -p build
-mkdir -p install
-
-#cmake .... && make -j && make install
-cd build
+cd $BUILD_DIR
 
 cmake -j4 .. -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
    -DCMAKE_C_COMPILER="${CC}" \
@@ -35,8 +36,10 @@ cmake -j4 .. -DCMAKE_INSTALL_PREFIX="${INSTALL_DIR}" \
    -DMPI_C_COMPILER="${MPICC}" \
    -DMPI_CXX_COMPILER="${MPICXX}" \
    -DMPI_Fortran_COMPILER="${MPIFC}" \
-   -DALLOW_MPI="ON" \
+   -DALLOW_MPI="OFF" \
    -DALLOW_CUDA="OFF" \
    -DALLOW_METAL="OFF" \
-   -DALLOW_OPENCL="OFF" \
+   -DALLOW_OPENCL="${ALLOW_OPENCL}" \
+   -DOpenCL_LIBRARY="${OpenCL_LIBRARY}" \
+   -DOpenCL_INCLUDE_DIR="${OpenCL_INCLUDE_DIR}" \
    && make -j4 && make install
